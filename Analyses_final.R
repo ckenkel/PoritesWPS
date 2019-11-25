3c63e8cb4c9f74f430954ac10a7d22ab634b3d04
ckenkel <ckenkel@usc.edu>
2019-11-25 14:00:29 -0800

    Add files via upload

--- Analyses_final.R
+++ Analyses_final.R
@@ -32,11 +32,13 @@ setwd("") #location of input files - fill in here
 #Read in data
 ########################
 
-dat=read.csv("Patchy_Porites_Feb2019_for_R.csv",stringsAsFactors=TRUE)
+dat=read.csv("Patchy_Porites_July2019_for_R.csv",stringsAsFactors=TRUE)
 
 summary(dat)
 
 dat$condition=factor(dat$condition,levels=c("healthy","WPS"))
+dat$GrossPS_mgO2.cm2.min<-(dat$dayO2_mgO2.cm2.min - dat$nightO2_mgO2.cm2.min)
+dat$PR<-(dat$GrossPS_mgO2.cm2.min/abs(dat$nightO2_mgO2.cm2.min))
 
 ###############plots
 
@@ -82,13 +84,13 @@ p2<-ggplot(dat,aes(factor(condition),chla_ug.cm2,fill=condition))+
 	theme(axis.text=element_text(size=11))+
 	theme(plot.subtitle=element_text(hjust=0.5))
 
-p3<-ggplot(dat,aes(factor(condition),GrossPS_mgO2.cm2.min,fill=condition))+
+p3<-ggplot(dat,aes(factor(condition),PR,fill=condition))+
 	geom_boxplot(outlier.shape=16, notch=FALSE)+
 	geom_point(aes(colour=factor(sid)),position=pd,show.legend=FALSE)+
 	geom_line(aes(group=factor(sid),colour=factor(sid)),position=pd,show.legend=FALSE)+
 	labs(colour="genet")+
 	#scale_colour_manual(values=c(point="gray60"))+
-	labs(subtitle="Gross Photosynthesis", y=expression(mgO["2"]/cm["2"]/min))+
+	labs(subtitle="Holobiont Productivity", y="P:R")+ #expression(mgO["2"]/cm["2"]/min)
 	xlab(NULL)+
 	#theme(axis.text.x=element_text(size=20))+
 	theme_classic()+
@@ -158,7 +160,7 @@ Fixed effects: chla_ug.cm2 ~ condition
 conditionbleach -18.42922  2.652398  7 -6.948136   2e-04
 
 #########################################################
-model3 <- lme(GrossPS_mgO2.cm2.min~condition,data=dat,random= ~1|sid,method="REML",na.action=na.omit) #change to different traits
+model3 <- lme(PR~condition,data=dat,random= ~1|sid,method="REML",na.action=na.omit) #change to different traits
 
 
 #Diagnostics to determine if models are any good to begin with
@@ -174,10 +176,31 @@ plot(model3,which=1) #want random scatter; no apparent trendline
 summary(model3)
 anova(model3)
 
-Fixed effects: GrossPS_mgO2.cm2.min ~ condition 
-                     Value Std.Error DF   t-value p-value
-(Intercept)      1.2017362 0.1252366  7  9.595729   0e+00
-conditionbleach -0.8978292 0.1508831  7 -5.950494   6e-04
+Fixed effects: PR ~ condition 
+                  Value Std.Error DF   t-value p-value
+(Intercept)   1.2535127 0.1141144  7 10.984703   0e+00
+conditionWPS -0.9119854 0.1613821  7 -5.651093   8e-04
+
+# model3 <- lme(GrossPS_mgO2.cm2.min~condition,data=dat,random= ~1|sid,method="REML",na.action=na.omit) #change to different traits
+
+
+# #Diagnostics to determine if models are any good to begin with
+# qqPlot(residuals(model3),xlab="Theoretical Quantiles",ylab="Observed Quantiles") #try a transform if data are non-normal
+
+# qqnorm(model3, ~ranef(., level=1))
+
+
+# #check homoscedascitity by plotting residuals against fitted values
+# plot(model3,which=1) #want random scatter; no apparent trendline
+
+
+# summary(model3)
+# anova(model3)
+
+# Fixed effects: GrossPS_mgO2.cm2.min ~ condition 
+                     # Value Std.Error DF   t-value p-value
+# (Intercept)      1.2017362 0.1252366  7  9.595729   0e+00
+# conditionbleach -0.8978292 0.1508831  7 -5.950494   6e-04
 
 #########################################################
 model4 <- lme(protein_mg.cm2~condition,data=dat,random= ~1|sid,method="REML",na.action=na.omit) #change to different traits
@@ -233,7 +256,7 @@ head(dat)
 #library(devtools)
 
  
-pca <- prcomp(dat[,c(4,6,9:11)],
+pca <- prcomp(dat[,c(4,6,9:10,12)],
       center = TRUE,
       scale. = TRUE) 
 summary(pca) #First 2 PCs explain 61% of the variation
@@ -245,14 +268,14 @@ scores[,1:2]
 dat[,1:2]
 
 library(ggfortify)
-df<-dat[,c(4,6,9:11)]
+df<-dat[,c(4,6,9:10,12)]
 pP<-autoplot(prcomp(df,center=TRUE,scale.=TRUE),data=dat,colour="condition",loadings=TRUE,loadings.colour="grey60",loadings.label=TRUE,loadings.label.size=3,loadings.label.colour="grey60")+scale_colour_manual(values=c("darkgreen","lightgreen"))+theme_bw()
 pG<-autoplot(prcomp(df,center=TRUE,scale.=TRUE),data=dat,colour="sid",loadings=TRUE,loadings.colour="grey60",loadings.label=TRUE,loadings.label.size=3,loadings.label.colour="grey60")+theme_bw()
 
-ggarrange(pP,pG,ncol=1,labels=c('a','b'),label.args=list(gp=grid::gpar(fint=4,cex=1.2)))
+ggarrange(pP,pG,ncol=2,labels=c('a','b'),label.args=list(gp=grid::gpar(fint=4,cex=1.2)))
 
 
-#1st PC explains 64% of the variation and distinguishes normal from WPS phenotypes, 2nd PC is a lot of genotype, but not totally
+#1st PC explains ~60% of the variation and distinguishes normal from WPS phenotypes, 2nd PC is a lot of genotype, but not totally
 
 #########################################################
 
